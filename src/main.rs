144e16747da7c3126abceb77774431fe7466fb02
Chris Lewis-Hou <chris@lewis-hou.dev>
2026-06-06 09:49:20 +1000

    Add required libraries, async main, logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::Env;
+use log::info;
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    info!("Hello, world!");
 }
