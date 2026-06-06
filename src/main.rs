3ddc238d77c0650ba337721af80073f6dcdf67f0
Chris Lewis-Hou <chris@lewis-hou.dev>
2026-06-06 14:34:34 +1000

    Add CLI argument handling, and updating of subdomains

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod context;
 use aws_sdk_route53::types::{
     Change, ChangeAction, ChangeBatch, ResourceRecord, ResourceRecordSet, RrType,
 };
+use clap::Parser;
 use context::{AwsErrorContext, ErrorContext};
 use env_logger::Env;
 use log::{Level, debug, error, info};
@@ -10,11 +11,41 @@ use std::process::exit;
 
 const CHECKIP_URL: &'static str = "https://checkip.amazonaws.com/";
 
+#[derive(Parser, Debug)]
+struct CliArgs {
+    #[arg(long)]
+    zone_name: String,
+    #[arg(long)]
+    subdomain: Vec<String>,
+    #[arg(long)]
+    update_root: bool,
+    #[arg(long, default_value_t = 300)]
+    interval: u32,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or(Level::Info.to_string())).init();
 
-    if let Err(e) = beacon("staticlinkage.dev".to_string()).await {
+    let args = CliArgs::parse();
+
+    let mut domains: Vec<String> = args
+        .subdomain
+        .iter()
+        .map(|subdomain| format!("{}.{}", subdomain, args.zone_name))
+        .collect();
+    if args.update_root {
+        domains.push(args.zone_name.clone());
+    }
+
+    if domains.len() == 0 {
+        error!(
+            "No domains were specified to update. Either supply at least one --subdomain or specify --update-root to update the root record."
+        );
+        exit(1);
+    }
+
+    if let Err(e) = beacon(args.zone_name, domains).await {
         error!("{e}");
         error!("Failed to update DNS. See error logs for details.");
         exit(1);
@@ -23,11 +54,11 @@ async fn main() {
     }
 }
 
-async fn beacon(host: String) -> Result<(), String> {
+async fn beacon(zone_name: String, domains: Vec<String>) -> Result<(), String> {
     let ip = get_ip().await.context("Failed to get IP")?;
     debug!("Resolved ip as: {ip}");
 
-    update_dns(host, ip).await?;
+    update_dns(zone_name, domains, ip).await?;
 
     Ok(())
 }
@@ -41,13 +72,13 @@ async fn get_ip() -> reqwest::Result<String> {
         .to_string())
 }
 
-async fn update_dns(host: String, ip: String) -> Result<(), String> {
+async fn update_dns(zone_name: String, domains: Vec<String>, ip: String) -> Result<(), String> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_route53::Client::new(&config);
 
-    let zone_id = get_zone_id(&client, &host).await?;
+    let zone_id = get_zone_id(&client, &zone_name).await?;
 
-    let change_batch = build_dns_change_batch(host, ip)?;
+    let change_batch = build_dns_change_batch(domains, ip)?;
 
     client
         .change_resource_record_sets()
@@ -84,28 +115,33 @@ async fn get_zone_id(client: &aws_sdk_route53::Client, host: &String) -> Result<
     Ok(zone_id.to_string())
 }
 
-fn build_dns_change_batch(host: String, ip: String) -> Result<ChangeBatch, String> {
-    let record = ResourceRecord::builder()
+fn build_dns_change_batch(domains: Vec<String>, ip: String) -> Result<ChangeBatch, String> {
+    let ip_record = ResourceRecord::builder()
         .value(ip)
         .build()
         .context("Failed to build ResourceRecord")?;
 
-    let record_set = ResourceRecordSet::builder()
-        .name(host.clone())
-        .r#type(RrType::A)
-        .ttl(300)
-        .resource_records(record)
-        .build()
-        .context("Failed to build ResourceRecordSet")?;
+    let mut changes: Vec<Change> = Vec::new();
+    for domain in domains {
+        let record_set = ResourceRecordSet::builder()
+            .name(domain)
+            .r#type(RrType::A)
+            .ttl(300)
+            .resource_records(ip_record.clone())
+            .build()
+            .context("Failed to build ResourceRecordSet")?;
 
-    let change = Change::builder()
-        .action(ChangeAction::Upsert)
-        .resource_record_set(record_set)
-        .build()
-        .context("Failed to build Change")?;
+        let change = Change::builder()
+            .action(ChangeAction::Upsert)
+            .resource_record_set(record_set)
+            .build()
+            .context("Failed to build Change")?;
+
+        changes.push(change);
+    }
 
     let change_batch = ChangeBatch::builder()
-        .changes(change)
+        .set_changes(Some(changes))
         .build()
         .context("Failed to build ChangeBatch")?;
 
